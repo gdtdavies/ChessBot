5aa746cefdafcc2088ae2bf110cdc7080f42b4e4
gdtdavies <gdtdavies@gmail.com>
2022-06-03 12:29:45 +0100

    piece can only make pseudo-legal moves

--- Source.cpp
+++ Source.cpp
@@ -59,6 +59,8 @@ void drawPiece(int x, int y, Colour c, Type t);
 void drawBoard();
 void drawSelected(int rank, int file);
 
+bitset<64> getLegalMoves(enumSquare sq, Colour c, Type t);
+
 Colour getPieceColour(int pos);
 Type getPieceType(int pos);
 
@@ -181,10 +183,10 @@ void mouseCallback(int button, int state, int x, int y) {
 					Wp.set(targetSq, 0); Wn.set(targetSq, 0); Wb.set(targetSq, 0); 
 					Wr.set(targetSq, 0); Wq.set(targetSq, 0); Wk.set(targetSq, 0);
 				}
-
+				if (!getLegalMoves(originSq, originColour, originType).test(targetSq)) return;
 				switch (originType) {
 				case p:
-					if (originColour == white) {
+					if (originColour == white) {	
 						Wp.set(originSq, 0);
 						Wp.set(targetSq, 1);
 					}
@@ -389,7 +391,7 @@ bitset<64> getLegalMoves(enumSquare sq, Colour c, Type t) {
 		}
 		else {
 			attacks.set(sq - 8, 1);
-			if (sq < 16 && !Occupied.test(sq - 8))
+			if (sq >= 48 && !Occupied.test(sq - 8))
 				attacks.set(sq - 16, 1);
 		}
 		break;
@@ -531,8 +533,8 @@ int main(int argc, char** argv) {
 	if (GLEW_OK != err)
 		std::cout << " GLEW ERROR" << std::endl;
 
-	//loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ");
-	loadFromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ");
+	loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ");
+	//loadFromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ");
 	//loadFromFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ");	
 	//loadFromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1 ");
 	//loadFromFen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1 ");
